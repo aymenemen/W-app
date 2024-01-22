8e21eee23c5b7c7839892001bca1ed70e5e8efe8
aymene <aymene.mene@gmail.com>
2024-01-22 22:51:40 +0100

    final

--- src/components/weatherapp/Weather.jsx
+++ src/components/weatherapp/Weather.jsx
@@ -1,41 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './weather.css'
 import search_icon from '../assets/search.png'
-import clear from '../assets/clear.png'
-import cloud from '../assets/cloud.png'
-import drizzle from '../assets/drizzle.png'
+import Clear from '../assets/clear.png'
+import Clouds from '../assets/cloud.png'
+import Drizzle from '../assets/drizzle.png'
 import humidity from '../assets/humidity.png'
-import rain from '../assets/rain.png'
-import snow from '../assets/snow.png'
+import Rain from '../assets/rain.png'
+import Snow from '../assets/snow.png'
 import wind from '../assets/wind.png'
-
+import axios from 'axios'
 const Weather = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      
+        
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7c57e3eb260be27dcb3bd24467103fae`);
+        
+        setCast(response.data);
+        setSrc(Clear)
+  
+    };
+
+    fetchData(); 
+  },[]);
+const [cast,setCast]=useState({})
+const [city,setCity]=useState('malaga')
+const [src,setSrc]=useState()
+const getCity=(e)=>{
+  setCity(e.target.value)
+}
+const Submit = async (e)=>{
+  e.preventDefault();
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7c57e3eb260be27dcb3bd24467103fae`);
+  setCast(response.data);
+  console.log(response.data)
+  img()
+  console.log(city)
+}
+
+function img(){
+  const weatherMain = cast?.weather[0]?.main;
+  console.log(weatherMain)
+  switch (weatherMain) {
+    case "Clouds":
+      setSrc(Clouds);
+      break;
+    case "Rain":
+      setSrc( Rain);
+      break;
+    case "Clear":
+      setSrc(Clear);
+      break;
+    case "Drizzle":
+      setSrc(Drizzle);
+      break;
+    case "Snow":
+      setSrc(Snow);
+      break;
+    default:
+      console.log("Weather condition not recognized. "+weatherMain);
+  }
+} 
   return (
     <div className='container'>
-    <div className="top-bar">
-        <input type="text" className="city" placeholder='Search Cities' />
+    <div>
+        <form className="top-bar" onSubmit={(e)=>Submit(e)}>
+        <input type="text" className="city" placeholder='Search Cities' onChange={getCity} />
         <div className="search-icon">
-            <img src={search_icon} alt="search icon" />
-        </div>
+           <button className='bW' type='submit'><img src={search_icon} alt="search icon" /></button> 
+        </div></form>
 </div>
 
         <div className="weather-img">
-            <img src={cloud} alt="" />
+            <img src={src} alt='weather' />
         </div>
-        <div className="temp">25</div>
-        <div className="city">rabat</div>
+        <div className="temp">{Math.floor((cast.main?.temp)-273.15)}°</div>
+        <div className="city">{cast.name}</div>
         <div className="data">
             <div className="element">
             <img src={humidity} alt="" className="icon" />
             <div className="info">
-              <div className="h-percent"></div>
+              <div className="h-percent">{cast.main?.humidity}%</div>
               <div className="text">Humidity</div>
             </div> 
             </div>
             <div className="element">
             <img src={wind} alt="" className="icon" />
             <div className="info">
-              <div className="w-speed"></div>
+              <div className="w-speed">{cast.wind?.speed}</div>
               <div className="text">Wind speed</div>
             </div>
               
